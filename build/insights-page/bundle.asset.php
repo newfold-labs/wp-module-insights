cdf46d4359e9901c54a94c69a527eba26729b45e
Vara <varaprasadmaruboina@gmail.com>
2026-04-16 16:31:30 +0530

    Updated the Drop-downs to match the bluehost components

--- build/insights-page/bundle.asset.php
+++ build/insights-page/bundle.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '09899dd7504a8ae7bc4e');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'react-jsx-runtime', 'wp-api-fetch', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '2281f107c96a6fda46ca');
